d1bbbef971062a81ee5ece562aaae1b55efa9eb8
sophiamarianoes <sophiasosomariano@gmail.com>
2024-09-12 11:46:16 -0300

    Add files via upload

---
+++ login/default.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <?php
+        echo "Primeiro PHP!<br>";
+        $nome = "Sophia Mariano";
+        echo $nome
+    ?>
+</body>
+</html>
